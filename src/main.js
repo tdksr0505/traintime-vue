125dc475cdb9300dcd8dfe1f2620865a98e80fbc
DESKTOP-1AKMAGM\Lucas <tdksr0505@gmail.com>
2021-12-19 17:33:00 +0800

    isDev改為false

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createMetaManager } from 'vue-meta';
 // import "./assets/css/bootstrap.css";
 
 const app = createApp(App);
-app.config.globalProperties.isDev = true;
+app.config.globalProperties.isDev = false;
 app.use(router);
 app.use(createMetaManager());
 app.mount('#app');
